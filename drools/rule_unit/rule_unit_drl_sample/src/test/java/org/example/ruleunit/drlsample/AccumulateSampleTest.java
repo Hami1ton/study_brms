1cd92c434f9c1ea5c2c7895611105939729d2ee9
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-07-30 14:39:39 +0900

    add pj

---
+++ drools/rule_unit/rule_unit_drl_sample/src/test/java/org/example/ruleunit/drlsample/AccumulateSampleTest.java
@@ -0,0 +1,47 @@
+package org.example.ruleunit.drlsample;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AccumulateSampleTest {
+
+    KieSession kieSession;
+
+    @BeforeEach
+    void initKieSession() {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kieContainer = ks.getKieClasspathContainer();
+        kieSession = kieContainer.newKieSession();
+    }
+
+    @AfterEach
+    void disposeKieSession() {
+        kieSession.dispose();
+    }
+
+    @Test
+    public void test_学年全体の平均点を計算する() {
+
+        // set up
+        var taro = new StudentTestScore("Taro", "A", 100);
+        var jiro = new StudentTestScore("Jiro", "A", 75);
+        var saburo = new StudentTestScore("Saburo", "B", 70);
+        kieSession.insert(taro);
+        kieSession.insert(jiro);
+        kieSession.insert(saburo);
+
+        // execute
+        int count = kieSession.fireAllRules();
+
+        // assert
+        assertEquals(count, 1);
+
+    }
+}
