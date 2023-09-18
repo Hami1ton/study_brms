c700fba2b2b80677548759d78dcbc540cbbcb2a2
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-09-18 13:46:19 +0900

    add pj

---
+++ drools/integrate_with_openai/unstructured_input/README.md
@@ -0,0 +1,9 @@
+# rule_unit_drl_sample
+
+Rule Unitのサンプルアプリ
+
+## 環境
+
+- jdk 17
+- drools 8.41.0.Final
+- Visual Studio Code 1.80.2
---
+++ drools/integrate_with_openai/unstructured_input/src/test/java/org/example/integratewithopenai/unstructuredinput/SampleTest.java
@@ -0,0 +1,27 @@
+package org.example.integratewithopenai.unstructuredinput;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.drools.ruleunits.api.RuleUnitInstance;
+import org.drools.ruleunits.api.RuleUnitProvider;
+
+public class SampleTest {
+    
+    @Test
+    public void test_ドリンクの決定() {
+        DrinkRuleUnit drinkRuleUnit = new DrinkRuleUnit();
+        RuleUnitInstance<DrinkRuleUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(drinkRuleUnit);
+
+        var person = new Person("Taro", 20);
+        drinkRuleUnit.getPersons().add(person);
+
+        // execute rule 
+        instance.fire();
+
+        // assert
+        var drink = drinkRuleUnit.getDrinkList().get(0);
+        assertEquals("Beer", drink.getName());
+
+        instance.close();
+    }
+}
