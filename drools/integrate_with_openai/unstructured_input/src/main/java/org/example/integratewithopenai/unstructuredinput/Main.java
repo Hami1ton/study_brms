082cc66a3d5aeeb3567d227851859f51690ba1c3
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-09-24 20:45:52 +0900

    本処理を追加

--- drools/integrate_with_openai/unstructured_input/src/main/java/org/example/integratewithopenai/unstructuredinput/Main.java
+++ drools/integrate_with_openai/unstructured_input/src/main/java/org/example/integratewithopenai/unstructuredinput/Main.java
@@ -6,13 +6,20 @@ import org.example.integratewithopenai.unstructuredinput.ruleunit.InquiryCategor
 public class Main {
 
     public static void main(String[] args) {
+        var customer = new Customer("Taro", 10);
+
+        // build prompt
+        StringBuilder sb = new StringBuilder("あなたはSaaS製品を手掛ける企業のカスタマーサービス担当です。これから私が送る文章を要望・質問・クレームのいずれかに分類してください。");
+        sb.append("回答は、要望であれば「A」、質問であれば「B」、クレームであれば「C」とアルファベット1文字で回答してください。\n");
+        sb.append("サービスのユーザ情報変更機能が使いづらいです。");
+        String prompt = sb.toString();
 
         // call API to get the inquiry category
+        AiApiClient ac = new AiApiClient();
+        InquiryCategory category = ac.decideCategory(prompt);
     
         // decide the priority 
         DecisionService ds = new DecisionService();
-        var customer = new Customer("Taro", 1);
-        var category = new InquiryCategory("A");
         var priority = ds.decidePriority(customer, category);
 
         System.out.println(priority);
