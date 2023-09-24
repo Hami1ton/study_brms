cd0f00e8e59cd04a86b324d4799d6dedbbe1e273
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-09-24 20:43:47 +0900

    update rule

--- drools/integrate_with_openai/unstructured_input/src/test/java/org/example/integratewithopenai/unstructuredinput/ruleunit/PriorityRuleUnitTest.java
+++ drools/integrate_with_openai/unstructured_input/src/test/java/org/example/integratewithopenai/unstructuredinput/ruleunit/PriorityRuleUnitTest.java
@@ -9,11 +9,11 @@ import org.drools.ruleunits.api.RuleUnitProvider;
 public class PriorityRuleUnitTest {
     
     @Test
-    public void test_優先度の決定() {
+    public void test_優先度の決定_長期顧客() {
         PriorityRuleUnit priorityRuleUnit = new PriorityRuleUnit();
         RuleUnitInstance<PriorityRuleUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(priorityRuleUnit);
 
-        var customer = new Customer("Taro", 1);
+        var customer = new Customer("Taro", 5);
         priorityRuleUnit.getCustomer().append(customer);
 
         var category = new InquiryCategory("A");
@@ -28,6 +28,54 @@ public class PriorityRuleUnitTest {
 
         // assert
         assertEquals(2, priority.value());
+
+        instance.close();
+    }
+
+    @Test
+    public void test_優先度の決定_中期顧客() {
+        PriorityRuleUnit priorityRuleUnit = new PriorityRuleUnit();
+        RuleUnitInstance<PriorityRuleUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(priorityRuleUnit);
+
+        var customer = new Customer("Taro", 4);
+        priorityRuleUnit.getCustomer().append(customer);
+
+        var category = new InquiryCategory("A");
+        priorityRuleUnit.getInquiryCategory().append(category);
+
+        // execute rule 
+        instance.fire();
+
+        // get result by query
+        var queryResult = instance.executeQuery("FindPriority").toList().get(0);
+        Priority priority = (Priority) queryResult.get("$p");
+
+        // assert
+        assertEquals(1, priority.value());
+
+        instance.close();
+    }
+
+    @Test
+    public void test_優先度の決定_短期顧客() {
+        PriorityRuleUnit priorityRuleUnit = new PriorityRuleUnit();
+        RuleUnitInstance<PriorityRuleUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(priorityRuleUnit);
+
+        var customer = new Customer("Taro", 1);
+        priorityRuleUnit.getCustomer().append(customer);
+
+        var category = new InquiryCategory("B");
+        priorityRuleUnit.getInquiryCategory().append(category);
+
+        // execute rule 
+        instance.fire();
+
+        // get result by query
+        var queryResult = instance.executeQuery("FindPriority").toList().get(0);
+        Priority priority = (Priority) queryResult.get("$p");
+
+        // assert
+        assertEquals(4, priority.value());
 
         instance.close();
     }
