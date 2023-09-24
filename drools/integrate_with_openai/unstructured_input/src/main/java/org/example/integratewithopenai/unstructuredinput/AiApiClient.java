db058d07a357586ec31a5dfccc410c70f23e3354
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-09-24 14:03:44 +0900

    add api client

--- drools/integrate_with_openai/unstructured_input/src/main/java/org/example/integratewithopenai/unstructuredinput/AiApiClient.java
+++ drools/integrate_with_openai/unstructured_input/src/main/java/org/example/integratewithopenai/unstructuredinput/AiApiClient.java
@@ -1,5 +1,33 @@
 package org.example.integratewithopenai.unstructuredinput;
 
+import java.util.ResourceBundle;
+
+import org.example.integratewithopenai.unstructuredinput.ruleunit.InquiryCategory;
+
+import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
 public class AiApiClient {
+
+    public InquiryCategory decideCategory(String prompt) {
+
+        // get token from env
+        String token = System.getenv("OPENAI_TOKEN");
+        OpenAiService service = new OpenAiService(token);        
+
+        CompletionRequest completionRequest = CompletionRequest.builder()
+            .prompt(prompt)
+            .model("gpt-3.5-turbo")
+            .echo(true)
+            .maxTokens(10)
+            .temperature(0.0)
+            .build();
+
+        CompletionChoice choice = service.createCompletion(completionRequest).getChoices().get(0);
+        System.out.println(choice.getText());
+
+        return new InquiryCategory(choice.getText());
+    }
     
 }
