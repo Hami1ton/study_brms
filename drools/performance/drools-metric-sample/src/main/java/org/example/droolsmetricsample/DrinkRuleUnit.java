7074b7948d2c8527dd59c81e85ee0f8d5ec03db1
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-11-18 16:08:54 +0900

    init pj

---
+++ drools/performance/drools-metric-sample/src/main/java/org/example/droolsmetricsample/DrinkRuleUnit.java
@@ -0,0 +1,36 @@
+package org.example.droolsmetricsample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.drools.ruleunits.api.DataSource;
+import org.drools.ruleunits.api.DataStore;
+import org.drools.ruleunits.api.RuleUnitData;
+
+public class DrinkRuleUnit implements RuleUnitData {
+    
+    private DataStore<Person> persons;
+
+    private List<Drink> drinkList = new ArrayList<>();
+
+    public DrinkRuleUnit() {
+        this(DataSource.createStore());
+    }
+
+    public DrinkRuleUnit(DataStore<Person> persons) {
+        this.persons = persons;
+    }
+
+    public void setPersons(DataStore<Person> persons) {
+        this.persons = persons;
+    }
+
+    public DataStore<Person> getPersons() {
+        return persons;
+    }
+
+    public List<Drink> getDrinkList() {
+        return drinkList;
+    }
+   
+}
