a6f0e23c8d631474376a25703f3d4b91f6052f6f
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-11-19 14:39:00 +0900

    add metric setting

--- drools/performance/drools-metric-sample/src/test/java/org/example/droolsmetricsample/SampleTest.java
+++ drools/performance/drools-metric-sample/src/test/java/org/example/droolsmetricsample/SampleTest.java
@@ -6,9 +6,14 @@ import org.drools.ruleunits.api.RuleUnitInstance;
 import org.drools.ruleunits.api.RuleUnitProvider;
 
 public class SampleTest {
+
     
     @Test
     public void test_ドリンクの決定() {
+        System.setProperty("drools.metric.logger.enabled", "true");
+        System.setProperty("drools.metric.logger.threshold", "1");
+        
+
         DrinkRuleUnit drinkRuleUnit = new DrinkRuleUnit();
         RuleUnitInstance<DrinkRuleUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(drinkRuleUnit);
 
