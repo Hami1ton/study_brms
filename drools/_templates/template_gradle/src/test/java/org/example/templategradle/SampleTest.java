34bf6b412de0c5fc1038085b5727b476f61e661b
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-10-19 16:33:24 +0900

    add pj

---
+++ drools/_templates/template_gradle/src/test/java/org/example/templategradle/SampleTest.java
@@ -0,0 +1,13 @@
+package org.example.templategradle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test_template() {
+        assertEquals(1, 1);
+    }
+
+}
