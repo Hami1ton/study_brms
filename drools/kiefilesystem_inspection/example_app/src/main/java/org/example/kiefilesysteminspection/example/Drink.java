6ad8b23a7259e3630d015b2becd2e06892f7298d
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-10-19 17:07:47 +0900

    add data objects

---
+++ drools/kiefilesystem_inspection/example_app/src/main/java/org/example/kiefilesysteminspection/example/Drink.java
@@ -0,0 +1,31 @@
+package org.example.kiefilesysteminspection.example;
+
+public class Drink {
+
+    private String name;
+
+    private int charge;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCharge(){
+        return charge;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCharge(int charge){
+        this.charge = charge;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + ", " + "charge: " + charge;
+    }
+    
+    
+}
