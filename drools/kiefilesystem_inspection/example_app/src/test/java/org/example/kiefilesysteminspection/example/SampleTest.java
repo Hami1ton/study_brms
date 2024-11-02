7c0119d86dc664bd5b0d23e0f41a24aa97c446eb
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-11-02 20:13:04 +0900

    update test

--- drools/kiefilesystem_inspection/example_app/src/test/java/org/example/kiefilesysteminspection/example/SampleTest.java
+++ drools/kiefilesystem_inspection/example_app/src/test/java/org/example/kiefilesysteminspection/example/SampleTest.java
@@ -2,12 +2,27 @@ package org.example.kiefilesysteminspection.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.kie.api.KieBase;
 
 public class SampleTest {
 
     @Test
-    public void test_template() {
-        assertEquals(1, 1);
-    }
+    public void test_ルール実行() {
+        var person = new Person();
+        person.setName("Taro");
+        person.setAge(22);
+        var drink = new Drink();
 
+        // set up Kie Session
+        KieBase kiebase = KieBaseCreater.create();
+        var kSession = kiebase.newKieSession();
+        // execute rule
+        kSession.insert(person);
+        kSession.insert(drink);
+        kSession.fireAllRules();
+
+        System.out.println(drink);
+
+        assertEquals(500, drink.getCharge());
+    }
 }
