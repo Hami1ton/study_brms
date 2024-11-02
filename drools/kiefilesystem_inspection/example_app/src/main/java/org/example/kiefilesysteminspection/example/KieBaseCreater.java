045018fbdea78cbc759fbc8da0f4f6b96b026e8b
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2024-11-02 20:12:30 +0900

    move kiebase creation logic

---
+++ drools/kiefilesystem_inspection/example_app/src/main/java/org/example/kiefilesysteminspection/example/KieBaseCreater.java
@@ -0,0 +1,52 @@
+package org.example.kiefilesysteminspection.example;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.KieRepository;
+import org.kie.api.runtime.KieContainer;
+
+public class KieBaseCreater {
+
+    public static KieBase create() {
+        KieServices ks = KieServices.Factory.get();
+        KieRepository kr = ks.getRepository();
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        kfs.write("src/main/resources/org/example/kiefilesysteminspection/example/Drink.drl", getRuleStr());
+        KieBuilder kb = ks.newKieBuilder(kfs);
+        kb.buildAll();
+
+        KieContainer kContainer = ks.newKieContainer(kr.getDefaultReleaseId());
+        return kContainer.getKieBase();
+    }
+
+    private static String getRuleStr() {
+        String rule = "" +
+                   "package org.example.kiefilesysteminspection.example \n" +
+                   "import org.example.kiefilesysteminspection.example.Person \n" +
+                   "import org.example.kiefilesysteminspection.example.Drink \n" +
+                   "rule \"Child\" \n" +
+                   "    when \n" +
+                   "        $person : Person( age < 20 ) \n" +
+                   "        $drink : Drink() \n" +
+                   "    then \n" +
+                   "        System.out.println(\"子供 ドリンク ルール発火\"); \n" +
+                   "        $drink.setName( \"Orange Juice\" ); \n" +
+                   "        $drink.setCharge( 200 ); \n" +
+                   "end \n" +
+
+                   "rule \"Adult\" \n" +
+                   "    when \n" +
+                   "        $person : Person( age >= 20 ) \n" +
+                   "        $drink : Drink() \n" +
+                   "    then \n" +
+                   "        System.out.println(\"大人 ドリンク ルール発火\"); \n" +
+                   "        $drink.setName( \"Beer\" ); \n" +
+                   "        $drink.setCharge( 500 ); \n" +
+                   "end \n";
+        return rule;
+    }
+    
+}
--- drools/kiefilesystem_inspection/example_app/src/main/java/org/example/kiefilesysteminspection/example/Main.java
+++ drools/kiefilesystem_inspection/example_app/src/main/java/org/example/kiefilesysteminspection/example/Main.java
@@ -1,11 +1,6 @@
 package org.example.kiefilesysteminspection.example;
 
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieRepository;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
+import org.kie.api.KieBase;
 
 public class Main {
 
@@ -16,7 +11,8 @@ public class Main {
         var drink = new Drink();
 
         // set up Kie Session
-        var kSession = kSessionFromStr(getRuleStr());
+        KieBase kiebase = KieBaseCreater.create();
+        var kSession = kiebase.newKieSession();
 
         // execute rule
         kSession.insert(person);
@@ -27,45 +23,5 @@ public class Main {
 
         // dispose
         kSession.dispose();
-    }
-
-    public static KieSession kSessionFromStr(String ruleStr) {
-        KieServices ks = KieServices.Factory.get();
-        KieRepository kr = ks.getRepository();
-        KieFileSystem kfs = ks.newKieFileSystem();
-
-        kfs.write("src/main/resources/org/example/kiefilesysteminspection/example/Drink.drl", ruleStr);
-        KieBuilder kb = ks.newKieBuilder(kfs);
-        kb.buildAll();
-
-        KieContainer kContainer = ks.newKieContainer(kr.getDefaultReleaseId());
-        KieSession kSession = kContainer.newKieSession();
-
-        return kSession;
-    }
-
-    private static String getRuleStr() {
-        String rule = "" +
-                   "package org.example.kiefilesysteminspection.example \n" +
-                   "import org.example.kiefilesysteminspection.example.Person \n" +
-                   "import org.example.kiefilesysteminspection.example.Drink \n" +
-                   "rule \"Child\" \n" +
-                   "    when \n" +
-                   "        $person : Person( age < 20 ) \n" +
-                   "        $drink : Drink() \n" +
-                   "    then \n" +
-                   "        $drink.setName( \"Orange Juice\" ); \n" +
-                   "        $drink.setCharge( 200 ); \n" +
-                   "end \n" +
-
-                   "rule \"Adult\" \n" +
-                   "    when \n" +
-                   "        $person : Person( age >= 20 ) \n" +
-                   "        $drink : Drink() \n" +
-                   "    then \n" +
-                   "        $drink.setName( \"Beer\" ); \n" +
-                   "        $drink.setCharge( 500 ); \n" +
-                   "end \n";
-        return rule;
     }
 }
