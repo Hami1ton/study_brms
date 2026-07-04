fe36a43927fb7ec850f2a1b0c29c70cc70c4bdac
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2026-07-04 14:48:37 +0900

    動いたところまでコミット

---
+++ drools/unit_test/drl_test/src/test/java/org/example/drltest/SingleFileTest.java
@@ -0,0 +1,44 @@
+package org.example.drltest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SingleFileTest {
+
+    static final Logger log = LoggerFactory.getLogger(SingleFileTest.class);
+
+    private KieServices ks = KieServices.Factory.get();
+
+    @Test
+    public void test_指定したDRLファイルのみ実行() {
+        // 特定のDRLファイルのみをリソースとして追加
+        KieFileSystem kfs = ks.newKieFileSystem();
+
+        // テストしたいDRLファイル（1ファイルだけ）をクラスパスから読み込む
+        kfs.write(ResourceFactory.newClassPathResource("org/example/drltest/Sample.drl"));
+        ks.newKieBuilder(kfs).buildAll();
+
+        KieContainer kContainer = ks.newKieContainer(ks.getRepository().getDefaultReleaseId());
+        KieSession kieSession = kContainer.newKieSession();
+
+        try {
+            var data = new SampleData();
+            kieSession.insert(data);
+            kieSession.fireAllRules();
+
+            // assert
+            assertEquals(3, data.getExecutedRules().size()); 
+
+        } finally {
+            kieSession.dispose();
+        }
+    }
+}
