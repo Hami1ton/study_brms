adab523ed792030d9c9d1b036aaad1bdaac0d188
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2026-07-04 17:34:22 +0900

    Create AllRuleTest.java

---
+++ drools/unit_test/drl_test/src/test/java/org/example/drltest/AllRuleTest.java
@@ -0,0 +1,42 @@
+package org.example.drltest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class AllRuleTest {
+
+    private KieContainer kieContainer;
+    private KieSession kieSession;
+
+    @BeforeEach
+    public void setup() {
+        KieServices ks = KieServices.Factory.get();
+        kieContainer = ks.getKieClasspathContainer();
+        kieSession = kieContainer.newKieSession();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        if (kieSession != null) {
+            kieSession.dispose();
+        }
+    }
+
+    @Test
+    public void test_全ルール実行のテスト() {
+        var data = new ExecutedRules();
+        kieSession.insert(data);
+        kieSession.fireAllRules();
+
+        // assert
+        assertEquals(Arrays.asList("Rule_A", "Rule_B", "Rule_C", "Rule_D"), data.getExecutedRules());
+    }
+}
